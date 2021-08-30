e4f02dddbb3c090a425538c3ec92ac39bd746ba4
Junio <junioalves2005@hotmail.com>
2021-08-30 18:30:20 -0300

    concluindo exercicio 1

--- script.js
+++ script.js
@@ -121,10 +121,70 @@
 
 //   console.log(Object.pais);
 
-const top3Albums = {
-    1: 'Nightfall in Middle-Earth',
-    2: 'Imaginations from the Other Side',
-    3: 'Somewhere Far Beyond',
-    };
-    console.log(Object.entries(top3Albums))
+// const top3Albums = {
+//     1: 'Nightfall in Middle-Earth',
+//     2: 'Imaginations from the Other Side',
+//     3: 'Somewhere Far Beyond',
+//     };
+//     console.log(Object.entries(top3Albums))
     
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  const customerInfo = (order) => {
+    const address = 'adress';
+    const deliveryPerson = order.order.delivery.deliveryPerson;
+    const name = order['name'];
+    const phoneNumber = order['phoneNumber'];
+    const street = order.address.street;
+    const number = order.address.number;
+    const apartment = order.address.apartment;
+
+    console.log(`Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phoneNumber}, R. ${street}, Nº${number}, AP: ${apartment}`)
+  }
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    const newName = order.name = 'Luiz Silva';
+    const pizza = Object.keys(order.order.pizza)
+    const coke = order.order.drinks.coke.type
+    const newValue = order.payment.total = 50
+
+    console.log(`Olà ${newName}, o total do seu pedido de ${pizza } e ${coke} é R$ ${newValue}.`)
+  }
+  
+  orderModifier(order);
